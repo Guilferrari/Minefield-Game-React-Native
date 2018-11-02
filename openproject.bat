349eef4f62b4bb0954db144042ff6949034aa278
Guilherme Ferrari <guilferrari@hotmail.com>
2018-11-02 19:39:15 +0100

    Project Mine Field React Native

---
+++ openproject.bat
@@ -0,0 +1,10 @@
+@echo off
+title openproject - by Lukas
+echo Welcome to open project! it's a simple piece of code which runs your react-native project faster then you by doing it manually.
+pause
+start code .
+start react-native run-android
+cd C:\Program Files\Android SDK\tools
+.\emulator -avd Emulator
+pause
+
